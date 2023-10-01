d524ff1fdaea3d7370f65b74ff8ae8648322c80a
Mihaly Nyilas <dr.dabzse@gmail.com>
2023-10-01 21:12:37 +0200

    something happens, something works... will continue later

--- crm/urls.py
+++ crm/urls.py
@@ -4,4 +4,16 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+#    path("login_user/", views.login_user, name="login_user"),
+    path("logout_user/", views.logout_user, name="logout_user"),
+    path("register_user/", views.register_user, name="register_user"),
 ]
+
+'''
+it works without the "login_user/" path
+reason:
+it is "wired" inside the home view's authentication
+
+if you wish, you can separate the login_user view,
+and code it's function/definition separately
+'''
--- crm/views.py
+++ crm/views.py
@@ -1,5 +1,71 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.contrib.auth.models import User
+
 
 # Create your views here.
 def home(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        # Authenticate user
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            # Login user
+            login(request, user)
+            messages.success(request, ("You have been logged in!"))
+            return redirect("home")
+        else:
+            messages.warning(request, ("Error logging in - Please try again..."))
+            return render(request, "home.html", {})
+            # return redirect("home.html")
+    else:
+        return render(request, "home.html", {})
+
+
+def logout_user(request):
+    logout(request)
+    messages.info(request, ("You have been logged out!"))
     return render(request, "home.html", {})
+
+
+def register_user(request):
+    if request.method == "POST":
+        # Get form values
+        first_name = request.POST["first_name"]
+        last_name = request.POST["last_name"]
+        username = request.POST["username"]
+        email = request.POST["email"]
+        password = request.POST["password"]
+        password_confirm = request.POST["password_confirm"]
+        # Check if passwords match
+        if password == password_confirm:
+            # Check username
+            if User.objects.filter(username=username).exists():
+                messages.warning(request, ("That username is already taken!"))
+                return render(request, "register.html", {})
+            elif User.objects.filter(email=email).exists():
+                messages.warning(request, ("That email is already being used!"))
+                return render(request, "register.html", {})
+            else:
+                # Looks good
+                user = User.objects.create_user(
+                    username=username,
+                    password=password,
+                    email=email,
+                    first_name=first_name,
+                    last_name=last_name,
+                )
+                # Login after register
+                # auth.login(request, user)
+                # messages.success(request, ("You have been logged in!"))
+                # return redirect("home")
+                user.save()
+                messages.success(request, ("You have been registered!"))
+                return render(request, "login.html", {})
+        else:
+            messages.error(request, ("Passwords do not match!"))
+            return render(request, "register.html", {})
+    else:
+        return render(request, "register.html", {})

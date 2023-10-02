a983e1324357a566a9e81037f02b5fc1b6f85508
Mihaly Nyilas <dr.dabzse@gmail.com>
2023-10-02 12:01:21 +0200

    add: data to models

--- crm/models.py
+++ crm/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Customer(models.Model):
+    username    =    models.CharField(max_length=30)
+    first_name  =    models.CharField(max_length=50)
+    last_name   =    models.CharField(max_length=50)
+    email       =    models.CharField(max_length=200)
+    created_at  =    models.DateTimeField(auto_now_add=True)
+    updated_at  =    models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.username}"
